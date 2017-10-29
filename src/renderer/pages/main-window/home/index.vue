aaaa2817366dd32f519a99fd534d27580b64c174
lyric <1353758518@qq.com>
2017-10-29 20:17:51 +0800

    element-ui升级到2.0正式版

--- src/renderer/pages/main-window/home/index.vue
+++ src/renderer/pages/main-window/home/index.vue
@@ -27,7 +27,8 @@ export default {
   name: 'home',
   data () {
     return {
-      flag: false
+      flag: false,
+      radio: '6'
     }
   },
   methods: {
@@ -37,38 +38,7 @@ export default {
     },
     // 在线文档
     onlineDocument () {
-      // global.__flag = !global.__flag
-      // console.info('__flag:', global.__flag)
       ipcRenderer.send('open-subWindow')
-      // this.flag = !this.flag
-      // if (this.flag) {
-      //   if (!subWin) {
-      //     const BrowserWindow = remote.BrowserWindow
-      //     subWin = new BrowserWindow({
-      //       width: 720,
-      //       height: 720,
-      //       x: mainWin.getPosition()[0] + mainWin.getSize()[0] + 5,
-      //       y: mainWin.getPosition()[1],
-      //       useContentSize: true,
-      //       resizable: false,
-      //       frame: false,
-      //       icon: path.join(remote.app.getAppPath(), 'favicon.ico')
-      //     })
-      //     subWin.setSkipTaskbar(true)
-      //     subWin.loadURL('http://localhost:9080/#/sub')
-      //     subWin.on('close', () => {
-      //       console.log('I do not want to be closed')
-      //       subWin = null
-      //       this.flag = false
-      //     })
-      //     // Open the DevTools.
-      //     // subWin.webContents.openDevTools()
-      //   } else {
-      //     subWin.show()
-      //   }
-      // } else {
-      //   subWin.hide()
-      // }
     }
   }
 }
